7ec1fc621f65d12e08c43788657c8863926a05b7
Christoph Steindl <c.steindl@univie.ac.at>
2015-12-13 23:37:15 +0100

    M pep

--- M.py
+++ M.py
@@ -17,18 +17,18 @@ str_3 = ""
 str_4 = ""
 
 for i in range(len(input_str)):
-	if (i % 6 ) == 0:
-		str_1 += input_str[i]
-	if (i % 6 ) == 1:
-		str_2 += input_str[i]
-	if (i % 6 ) == 2:
-		str_3 += input_str[i]
-	if (i % 6 ) == 3:
-		str_4 += input_str[i]
-	if (i % 6 ) == 4:
-		str_3 += input_str[i]
-	if (i % 6 ) == 5:
-		str_2 += input_str[i]
+    if (i % 6) == 0:
+        str_1 += input_str[i]
+    if (i % 6) == 1:
+        str_2 += input_str[i]
+    if (i % 6) == 2:
+        str_3 += input_str[i]
+    if (i % 6) == 3:
+        str_4 += input_str[i]
+    if (i % 6) == 4:
+        str_3 += input_str[i]
+    if (i % 6) == 5:
+        str_2 += input_str[i]
 
 
 output_str = str_1 + str_2 + str_3 + str_4
