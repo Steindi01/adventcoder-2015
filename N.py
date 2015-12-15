7d104b5d82e4605dc6e80bee67fe8f561c269617
Christoph Steindl <c.steindl@univie.ac.at>
2015-12-15 12:27:14 +0100

    second attempt for N

--- N.py
+++ N.py
@@ -11,38 +11,27 @@ input_str = my_input[0]
 input_str = input_str.replace("?", ".")
 input_str = input_str.replace("!", ".")
 errors = 0
-splits = [i for i in range(len(input_str)) if input_str.startswith('.', i)]
+sentences = input_str.split(". ")
 
-sentences = []
-last_start = 0
-for i in splits:
-    if i + 1 == len(input_str):
-        sentences.append(input_str[last_start:i + 1])
-        break
-
-    if input_str[i + 1] == " ":
-        sentences.append(input_str[last_start:i + 1])
-        last_start = i + 2
-
-if splits[-1] + 1 != len(input_str):
-    sentences.append(input_str[last_start:len(input_str)])
+if sentences[-1][-1] != ".":
+    errors += 1
+else:
+    sentences[-1] = sentences[-1][:-1]
 
 for s in sentences:
-    if s[-1] != ".":
+    if len(s) == 0:
         errors += 1
-    if s[0] != s[0].upper():
-        errors += 1
-
-    words = s[:-1].split(" ")
-    for w in words:
-        for i in range(len(w)):
-            if w[i] == ".":
-                errors += 1
-            if i > 0 and w[i] == w[i].upper():
-                errors += 1
-
-        if len(w) == 0:
+    else:
+        if s[0] != s[0].upper():
             errors += 1
-
+        for word in s.split(" "):
+            if len(word) == 0:
+                errors += 1
+            else:
+                for i in range(len(word)):
+                    if (i >= 1) and (word[i] == word[i].upper()):
+                        errors += 1
+                    if word[i] == ".":
+                        errors += 1
 
 print errors
